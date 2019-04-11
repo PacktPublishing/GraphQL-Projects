6e6b631aad6bd406d6490bbafaf521db5be3283c
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-11 14:17:40 +0800

    add styling for job and markdown rendering

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "react-apollo-hooks";
+import ReactMarkdown from "react-markdown";
+import styled from "styled-components";
+import { parseMarkdown } from "../markdownUtils";
+import { Link } from "@reach/router";
 
 const GET_JOB = gql`
   {
@@ -8,8 +12,28 @@ const GET_JOB = gql`
       id
       company
       description
+      link_to_apply
     }
   }
+`;
+
+const ApplyLink = styled.a`
+  padding: 10px;
+  background: red;
+  color: white;
+  text-decoration: none;
+`;
+const ApplyContainer = styled.div`
+  text-align: center;
+  width: 100%;
+`;
+
+const BackButton = styled(Link)`
+  color: gray;
+`;
+
+const JobContainer = styled.div`
+  margin: 20px;
 `;
 
 function Job(params) {
@@ -17,12 +41,17 @@ function Job(params) {
   if (loading) {
     return <h1>Loading...</h1>;
   } else if (data) {
-    const { description, id, company } = data.jobs_by_pk;
+    const { description, id, company, link_to_apply } = data.jobs_by_pk;
+    let convertedDescription = parseMarkdown(description);
     return (
-      <div>
+      <JobContainer>
+        <BackButton to="/"> Back to search </BackButton>
         <h1>Work for {company}</h1>
-        <span>{description}</span>
-      </div>
+        <ReactMarkdown source={convertedDescription} />
+        <ApplyContainer>
+          <ApplyLink href={link_to_apply}>Apply Now</ApplyLink>
+        </ApplyContainer>
+      </JobContainer>
     );
   }
 }
