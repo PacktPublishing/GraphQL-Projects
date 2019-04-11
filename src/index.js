ae5cbc969e96f87f99e19c0b3bb9dcead00ac330
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-11 12:20:41 +0800

    Testing local install - moving components into src to avoid errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import Jobs from "../components/Jobs";
+import Jobs from "./components/Jobs";
 import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   uri:
-    "https://jb-simpler.herokuapp.com/v1alpha1/graphql" /* Example Static GraphQL Backend */
+    "https://jb-simpler.herokuapp.com/v1alpha1/graphql" /* Example Static GraphQL Backend */,
 });
 
 const App = () => (
