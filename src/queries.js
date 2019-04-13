d4c3d10902644786ae39c513133ed9ccc7cbf0f1
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-13 15:47:08 +0800

    Add create new job

--- src/queries.js
+++ src/queries.js
@@ -20,3 +20,25 @@ export const GET_JOB = gql`
     }
   }
 `;
+
+export const CREATE_NEW_JOB = gql`
+  mutation CreateJob(
+    $company: String!
+    $title: String!
+    $link_to_apply: String!
+    $description: String!
+  ) {
+    insert_jobs(
+      objects: {
+        company: $company
+        title: $title
+        link_to_apply: $link_to_apply
+        description: $description
+      }
+    ) {
+      returning {
+        id
+      }
+    }
+  }
+`;
