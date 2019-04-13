2b50e90851263c66a84facb167faa8f99022a303
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-13 14:59:38 +0800

    Support parsing both string and json

--- src/markdownUtils.js
+++ src/markdownUtils.js
@@ -1,4 +1,13 @@
 const md = require("md-2-json");
-export const fromDb = markdownFromDb => md.toMd(JSON.parse(markdownFromDb));
+export const fromDb = markdownFromDb => {
+  let markdownString = markdownFromDb;
+  try {
+    markdownString = JSON.parse(markdownFromDb);
+    return md.toMd(markdownString);
+  } catch (SyntaxError) {
+    // presume it is not a json and saved as string instead
+    return markdownString;
+  }
+};
 export const toDb = stringToBeConverted =>
   JSON.stringify(md.parse(stringToBeConverted));
