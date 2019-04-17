0ed3e72fbdc0d0e69ec53bbe4088e2e6e1cef9b1
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-17 21:35:40 +0800

    components split into separate files

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,70 +1,6 @@
-import React, { useState } from "react";
-import { useMutation } from "react-apollo-hooks";
-import { ADD_MESSAGE, GET_MESSAGES, UPDATE_MESSAGE } from "../queries";
-import { format } from "timeago.js";
-
-function Message({ id, username: name, timestamp: time, text: message }) {
-  const [hovered, setHovered] = useState(false);
-  const [editMode, setEditMode] = useState(false);
-  const [messageState, setMessageState] = useState(message);
-  const updateMessage = useMutation(UPDATE_MESSAGE, {});
-  if (editMode) {
-    return (
-      <div className="flex flex-wrap">
-        <div className="w-full">
-          <textarea
-            className="w-full"
-            value={messageState}
-            onChange={e => setMessageState(e.target.value)}
-          />
-        </div>
-        <div className="flex">
-          <input
-            type="button"
-            className="py-1 px-1 rounded bg-gray-300"
-            onClick={() => setEditMode(false)}
-            value="Cancel"
-          />
-          <input
-            type="button"
-            onClick={() =>
-              updateMessage({
-                variables: { id, text: messageState },
-              }).then(() => setEditMode(false))
-            }
-            className="bg-green-500 py-1 px-2 rounded ml-2"
-            value="Save"
-          />
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        key={id}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className="flex items-start mb-4 text-sm"
-      >
-        <div className="flex-1 overflow-hidden">
-          <div>
-            <span className="font-bold">{name}</span>
-            <span className="p-1 text-gray-500 text-xs">{format(time)}</span>
-            {hovered && (
-              <input
-                type="button"
-                onClick={() => setEditMode(true)}
-                className="py-1 px-1 rounded float-right"
-                value="Edit"
-              />
-            )}
-          </div>
-          <p className="text-black leading-normal">{message}</p>
-        </div>
-      </div>
-    );
-  }
-}
+import React from "react";
+import Message from "./Message";
+import NewMessage from "./NewMessage";
 
 function ChatWindow({ messages }) {
   const renderedMessages = messages.map(message => (
@@ -75,48 +11,8 @@ function ChatWindow({ messages }) {
       <div className="px-6 py-4 flex-1 overflow-y-scroll">
         {renderedMessages}
       </div>
-      <SubmitMessage />
+      <NewMessage />
     </React.Fragment>
-  );
-}
-
-function SubmitMessage() {
-  const addMessage = useMutation(ADD_MESSAGE, {
-    refetchQueries: [{ query: GET_MESSAGES }],
-  });
-  const [message, setMessage] = useState("");
-  const handleSubmission = e => {
-    e.preventDefault();
-    addMessage({
-      variables: {
-        text: message,
-        username: "maxim",
-      },
-    }).then(() => setMessage(""));
-  };
-  return (
-    <div className="pb-6 px-4 flex-none">
-      <div className="flex rounded-lg border-2 border-gray-500 border-solid overflow-hidden">
-        <span className="border-0 border-solid text-3xl text-gray-500 border-r-2 border-gray-500 p-2">
-          <svg
-            className="fill-current h-6 w-6 block"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M16 10c0 .553-.048 1-.601 1H11v4.399c0 .552-.447.601-1 .601-.553 0-1-.049-1-.601V11H4.601C4.049 11 4 10.553 4 10c0-.553.049-1 .601-1H9V4.601C9 4.048 9.447 4 10 4c.553 0 1 .048 1 .601V9h4.399c.553 0 .601.447.601 1z" />
-          </svg>
-        </span>
-        <form className="flex w-full" onSubmit={handleSubmission}>
-          <input
-            type="text"
-            className="w-full px-4"
-            placeholder="Message #general"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-          />
-        </form>
-      </div>
-    </div>
   );
 }
 
---
+++ src/components/NewMessage.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useMutation } from "react-apollo-hooks";
+import { ADD_MESSAGE, GET_MESSAGES } from "../queries";
+
+function NewMessage() {
+  const addMessage = useMutation(ADD_MESSAGE, {
+    refetchQueries: [{ query: GET_MESSAGES }],
+  });
+  const [message, setMessage] = useState("");
+  const handleSubmission = e => {
+    e.preventDefault();
+    addMessage({
+      variables: {
+        text: message,
+        username: "maxim",
+      },
+    }).then(() => setMessage(""));
+  };
+  return (
+    <div className="pb-6 px-4 flex-none">
+      <div className="flex rounded-lg border-2 border-gray-500 border-solid overflow-hidden">
+        <span className="border-0 border-solid text-3xl text-gray-500 border-r-2 border-gray-500 p-2">
+          <svg
+            className="fill-current h-6 w-6 block"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M16 10c0 .553-.048 1-.601 1H11v4.399c0 .552-.447.601-1 .601-.553 0-1-.049-1-.601V11H4.601C4.049 11 4 10.553 4 10c0-.553.049-1 .601-1H9V4.601C9 4.048 9.447 4 10 4c.553 0 1 .048 1 .601V9h4.399c.553 0 .601.447.601 1z" />
+          </svg>
+        </span>
+        <form className="flex w-full" onSubmit={handleSubmission}>
+          <input
+            type="text"
+            className="w-full px-4"
+            placeholder="Message #general"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default NewMessage;
