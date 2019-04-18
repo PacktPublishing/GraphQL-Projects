c95fb00eabecb3bb51265566086c68ec9edfa7d8
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-18 16:20:47 +0800

    Wiring up auth0

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import { GET_USER, ADD_USER } from "../queries";
 import { useMutation, useQuery } from "react-apollo-hooks";
 
-function Login({ onLogin }) {
-  const [login, setLogin] = useState("");
-  const [loggingIn, setLoggingIn] = useState(false);
-  const addUser = useMutation(ADD_USER, { variables: { username: login } });
+function Login({ usernameSelected }) {
+  const [username, setUsername] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const addUser = useMutation(ADD_USER, { variables: { username: username } });
 
   const { data: userData, loading: userLoading } = useQuery(GET_USER, {
-    variables: { username: login },
-    skip: !loggingIn,
+    variables: { username: username },
+    skip: !creatingUser,
   });
 
-  if (loggingIn && userData && userData.matching_user) {
+  if (creatingUser && userData && userData.matching_user) {
     if (userData.matching_user.matches.count === 0) {
       addUser().then(() => {
-        onLogin(login);
+        usernameSelected(username);
       });
     } else {
-      onLogin(login);
+      usernameSelected(username);
     }
   }
-  if (userLoading || loggingIn) {
+  if (userLoading || creatingUser) {
     return <h1>Loading...</h1>;
   }
   return (
@@ -29,14 +29,14 @@ function Login({ onLogin }) {
       <form
         onSubmit={e => {
           e.preventDefault();
-          setLoggingIn(true);
+          setCreatingUser(true);
         }}
       >
         <input
           className="rounded px-2 py-2"
-          onChange={e => setLogin(e.target.value)}
+          onChange={e => setUsername(e.target.value)}
           type="text"
-          value={login}
+          value={username}
           placeholder="Choose a username"
         />
       </form>
