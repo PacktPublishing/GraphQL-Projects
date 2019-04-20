2200e14e731c3bc05c19753424d9d782da93c02f
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-20 16:35:54 +0800

    Add initial version of adding vehicle

--- src/components/RouteManager/index.jsx
+++ src/components/RouteManager/index.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import RouteSelector from "./RouteSelector";
-import { Form, Box, Text, RangeInput, Button } from "grommet";
+import { Form, Box, Text, RangeInput, Button, FormField } from "grommet";
+import { useMutation } from "react-apollo-hooks";
+import { ADD_VEHICLE, ADD_LOCATION } from "../../queries";
 
 const RouteManager = () => {
   const [route, setRoute] = useState("");
   const [updateInterval, setUpdateInterval] = useState(10);
+  const [name, setName] = useState("");
+  const [polls, setPolls] = useState([]);
+  const addVehicle = useMutation(ADD_VEHICLE);
+  const handleSubmit = e => {
+    e.preventDefault();
+    addVehicle({
+      variables: {
+        name,
+      },
+    }).then(x => {
+      const vehicleId = x.returning.id;
+      const newInterval = setInterval(() => {
+        console.log("ADDING LOCATION for vehicle ", vehicleId);
+      }, updateInterval);
+      setPolls(polls.concat([{ poll: newInterval, vehicleId: vehicleId }]));
+    });
+  };
 
   return (
     <Box align="center">
       <h1>Manage routes</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Box width="medium" pad="medium" gap="medium">
+          <FormField
+            label="Vehicle name"
+            onChange={e => setName(e.target.value)}
+          />
           <label>Select a route:</label>
           <RouteSelector
             value={route}
