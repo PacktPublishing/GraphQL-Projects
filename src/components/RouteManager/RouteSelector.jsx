464e48f223b8a35a3df7afe0f275a654c9edcef1
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-20 15:49:45 +0800

    Add simple form with route and interval selection

---
+++ src/components/RouteManager/RouteSelector.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Select } from "grommet";
+const routes = require("../../data/routes.json");
+
+const RouteSelector = props => {
+  return <Select {...props} options={routes.routes.map(x => x.name)} />;
+};
+
+export default RouteSelector;
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Router } from "@reach/router";
+import RouteManager from "./RouteManager";
+import { Grommet } from "grommet";
 
-const RouteManager = () => <h1>Manage routes</h1>;
 const RouteViewer = () => <h1>View routes</h1>;
 const Routes = () => (
-  <Router>
-    <RouteViewer path="/" />
-    <RouteManager path="/manage" />
-  </Router>
+  <Grommet plain>
+    <Router>
+      <RouteViewer path="/" />
+      <RouteManager path="/manage" />
+    </Router>
+  </Grommet>
 );
 
 export default Routes;
