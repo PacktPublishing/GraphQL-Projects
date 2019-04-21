ebe20de1b8e5bff577f95072cd372fb13e14ea6e
Maxim Filimonov <tpaktopsp@gmail.com>
2019-04-21 17:27:59 +0800

    Wire up apollo websocket transport for subs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,41 @@ import React from "react";
 import Routes from "./components/Routes";
 import { ApolloProvider } from "react-apollo";
 import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
-import ApolloClient from "apollo-boost";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import ApolloClient from "apollo-client";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+const WEBSOCKET_ENDPOINT =
+  "ws://gql-projects-vehicle-tracking.herokuapp.com/v1alpha1/graphql";
+const HTTP_ENDPOINT =
+  "https://gql-projects-vehicle-tracking.herokuapp.com/v1alpha1/graphql";
+
+const wsLink = new WebSocketLink({
+  uri: WEBSOCKET_ENDPOINT,
+  options: {
+    reconnect: true,
+  },
+});
+const httpLink = new HttpLink({
+  uri: HTTP_ENDPOINT,
+});
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink,
+);
 
 const client = new ApolloClient({
-  uri: "https://gql-projects-vehicle-tracking.herokuapp.com/v1alpha1/graphql",
+  link,
+  cache: new InMemoryCache(),
 });
 
 const App = () => (
